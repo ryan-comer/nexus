1f3436225c901f22db84dc472ac9c20a93074544
Ryan Comer <ryancomer94@gmail.com>
2024-04-19 08:20:13 -0500

    Boilerplate for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import BotsPage from './pages/BotsPage';
+import MainBar from './components/MainBar';
+
+const routes = [
+  {
+    path: "/",
+    component: () => <HomePage/>,
+    name: "Home"
+  },
+  {
+    path: "/bots",
+    component: () => <BotsPage/>,
+    name: "Bots"
+  }
+]
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <MainBar routes={routes}/>
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.component()}/>
+          ))}
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
